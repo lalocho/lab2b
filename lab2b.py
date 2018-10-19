90a5d0115cefe93d5b752b42b609acb456c84b9a
luis ochoa <lalocho13@gmail.com>
2018-10-18 20:04:37 -0700

    Add files via upload

--- lab2b.py
+++ lab2b.py
@@ -1,5 +1,5 @@
-## Luis Ochoa
-##80508534
+# Luis Ochoa
+# 80508534
 
 
 class Node(object):
@@ -10,97 +10,123 @@ class Node(object):
         self.password = password
         self.count = count
         self.next = next
-##creates linked list with passwords and and sorts them in ascending order using bubble sort
+
+
+# creates linked list with passwords and and sorts them in ascending order using bubble sort
 def solution_A():
     f = open('test.txt')
     line = f.readline()
     list = Node(None,-1,None)
-    while line:
-        list = insert_A((line.split(None, 1)[1]).rstrip(),list)
+    while line:  # reads line by line to not run out of memory; gets only the passwords
+        list = insert_A((line.split(None, 1)[1]).rstrip(), list)
         line = f.readline()
 
     f.close()
-    ##print_list(list)
+    # print_list(list)
+
     list = bubble_sort(list)
     print_top_20(list)
-## creates dictionary of passwords then dumps into a linked list to sort it using merge sort
+
+
+# creates dictionary of passwords then dumps into a linked list to sort it using merge sort
 def solution_B():
+
     f = open('test.txt')
     line = f.readline()
     dict = {}
     while line:
-        ##getting only the passwords from each line
+        # getting only the passwords from each line
         dict = insert_B((line.split(None, 1)[1]).rstrip(), dict)
         line = f.readline()
 
     f.close()
-    ## dumping dictionary into list
+    # dumping dictionary into list
     list = dict_to_node(dict)
     list = merge_sort(list)
     print_top_20(list)
-    ##print_list(list)
-## used to obtain the length of a list
+    # print_list(list)
+
+
+# used to obtain the length of a list
 def lenlist(list):
-    count = 0;
-    while list != None:
+
+    count = 0
+    while list is not None:
         count += 1
         list = list.next
     return count
-## used to test contents of lists/nodes
+
+
+# used to test contents of lists/nodes
 def print_list(list):
-    while list != None :
+
+    while list is not None :
         print (list.password, list.count)
         list = list.next
-## used by solution a to insert items into the list
-def insert_A(str,list):
+
+
+# used by solution a to insert items into the list
+def insert_A(str, list):
+
     temp_node = list
-    ##if it finds password it adds to counter, otherwise creates new node
-    while temp_node.password != None:
+    # if it finds password it adds to counter, otherwise creates new node
+    while temp_node.password is not None:
         if temp_node.password == str:
             temp_node.count += 1
             return list
         temp_node = temp_node.next
-    new_node = Node(str,0,list)
+    new_node = Node(str, 0, list)
     list = new_node
     return list
-def insert_B(str,dict):
+
+
+# checks if password is present in dictionary and adds to counter, if not creates new bucket
+def insert_B(str, dict):
+
     if str in dict:  # You can assume this operation takes O(1)
         dict[str] = dict[str] + 1
     else:
         dict[str] = 0
 
     return dict
-##dumps dictionary and returns a linked list
+
+
+# dumps dictionary and returns a linked list
 def dict_to_node(dict):
-    temp_node = Node(None,-1,None)
+
+    temp_node = None
     for key, val in dict.items():
         node_helper = Node(key,val,temp_node)
         temp_node = node_helper
     return temp_node
+
+
 def bubble_sort(list):
 
-    node_helper = list
     for i in range(lenlist(list)-1):
+        # resets tempnodes to iterate through linked list again
         temp_node1 = list
         temp_node2 = list.next
 
         for j in range(lenlist(list)-1):
 
-            if (temp_node1.count) < (temp_node2.count):
-                ##changing contents of nodes if they are in wrong order
+            if temp_node1.count < temp_node2.count:
+                # changing contents of nodes if they are in wrong order
                 node_helper_count = temp_node1.count
                 node_helper_password = temp_node1.password
-                temp_node1.password  = temp_node2.password
+                temp_node1.password = temp_node2.password
                 temp_node1.count = temp_node2.count
                 temp_node2.password = node_helper_password
                 temp_node2.count = node_helper_count
-            ##progressing through linked list
+            # progressing through linked list
             temp_node2 = temp_node2.next
             temp_node1 = temp_node1.next
     return list
 
+
 def merge_sort(list):
-    if list == None or list.next== None or list.next.next == None:
+    # basa case
+    if list is None or list.next is None or list.next.next is None:
         return list
     mid = lenlist(list)//2
     right = None
@@ -110,33 +136,42 @@ def merge_sort(list):
 
     for i in range(mid):
         node_helper = node_helper.next
-
+    # cutting linked list in half
     right = node_helper.next
-    node_helper.next = None;
+    node_helper.next = None
+    # merging back
     left = merge_sort(left)
     right = merge_sort(right)
-    list = merge(left,right)
+    list = merge(left, right)
     return list
-def merge(left,right):
-    result= None
-    if left == None:
+
+
+def merge(left, right):
+
+    result = None
+    if left is None:
         return right
-    if right == None:
+    if right is None:
         return left
     if left.count > right.count:
         result = left
-        result.next = merge(left.next,right)
+        result.next = merge(left.next, right)
     else:
         result = right
-        result.next = merge(left,right.next)
+        result.next = merge(left, right.next)
     return result
 
-    return
+
 def print_top_20(list):
 
     for i in range(20):
-        print(i+1,list.password,"->", list.count)
+        print(i+1, list.password, "->", list.count)
         list = list.next
 
-solution_A()
-solution_B()
+
+def main():
+
+    solution_A()
+    solution_B()
+
+main()
